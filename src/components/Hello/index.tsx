6ab5af605a20fc9536c62ce93d1b43a7c1ff4fbb
robby.hong <robby.hong@clinico.com.tw>
2022-08-26 14:24:55 +0800

    add tailwind

--- src/components/Hello/index.tsx
+++ src/components/Hello/index.tsx
@@ -2,27 +2,9 @@ import logo from '~/logo.svg';
 
 const Hello = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        paddingTop: '50px',
-      }}
-    >
-      <img src={logo} alt='Logo' style={{ height: '150px' }} />
-      <ul
-        style={{
-          padding: 0,
-          listStyle: 'none',
-          fontSize: '1.6rem',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '14px',
-        }}
-      >
+    <div className=' flex flex-col justify-center items-center h-screen'>
+      <img src={logo} alt='Logo' className='h-40' />
+      <ul>
         <li>🚀 Vite</li>
         <li>🔥 React</li>
         <li>📖 TypeScript</li>
@@ -32,7 +14,7 @@ const Hello = () => {
       <p>Don&apos;t forgot to install Eslint and Prettier in your VSCode</p>
       <div>
         <a
-          style={{ color: '#F24C4C' }}
+          className='text-[gray] text-title'
           target='_blank'
           href='https://github.com/igdev116/vite-react-ts-eslint-prettier'
           rel='noreferrer'
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/* eslint-env node */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        example: {
+          blue: '#0E5DC1',
+          gray: '#434447',
+        },
+      },
+      fontSize: {
+        h1: ['36px', { lineHeight: '46.8px' }],
+        h2: ['24px', { lineHeight: '31.2px' }],
+        h3: ['20px', { lineHeight: '26px' }],
+        title: ['28px', { lineHeight: '36.4px' }],
+        'social-icon-size': ['24px'],
+        '16-24': ['16px', { lineHeight: '24px' }],
+        'footer-text': ['14px', { lineHeight: '23.8px' }],
+      },
+    },
+  },
+  plugins: [],
+};
